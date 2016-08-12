c17bb6211ed20c5fbd42489d77bca005dea032e1
trstones <trstones@colby.edu>
2016-08-12 13:57:08 -0400

    Initial Project
    From Mark’s USB drive, plus a few graphical additions.

---
+++ Neuron/Assets/Event3.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event3 : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "Neuron", if it is...
+        if (other.gameObject.CompareTag("Neuron"))
+        {
+            other.gameObject.transform.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
