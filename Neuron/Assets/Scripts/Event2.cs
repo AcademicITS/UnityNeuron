20c308a3db7974cd7b6c1270ccddc04eebd7e790
trstones <trstones@colby.edu>
2016-08-18 16:08:35 -0400

    Neuron points

---
+++ Neuron/Assets/Scripts/Event2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event2 : MonoBehaviour {
+
+	public GameObject NeuronBase;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "Neuron", if it is...
+        if (other.gameObject.CompareTag("Neuron"))
+        {
+			NeuronBase.GetComponent<SpriteRenderer>().material.color = Color.blue;
+        }
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		{
+			NeuronBase.GetComponent<SpriteRenderer>().material.color = Color.white;
+		}
+	}
+}
