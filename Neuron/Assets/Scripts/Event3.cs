88683d1a811ea80c2829e649108cb7e631afe0fc
mwardeck <mwardeck@colby.edu>
2016-08-24 10:00:31 -0400

    Destroy
    This removes Na+ from the neuron when Glu is dragged away from a binding
    point. I've also made the Na+ buttons draggable.

--- Neuron/Assets/Scripts/Event3.cs
+++ Neuron/Assets/Scripts/Event3.cs
@@ -5,7 +5,7 @@ public class Event3 : MonoBehaviour {
 
 	public GameObject NeuronBase;
     public GameObject Na;
-
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         //Check the provided Collider2D parameter other to see if it is tagged "Neuron", if it is...
@@ -14,13 +14,15 @@ public class Event3 : MonoBehaviour {
 			NeuronBase.GetComponent<SpriteRenderer>().material.color = Color.yellow;
             //Clone Na within the Neuron body//
             Instantiate(Na, new Vector3 (-0.49f, .76f, .2754617f), Quaternion.Euler (0,0,0));
-        }
+            }
 	}
 
 	void OnTriggerExit2D(Collider2D other)
 	{
 		{
-			NeuronBase.GetComponent<SpriteRenderer>().material.color = Color.white;
+            NeuronBase.GetComponent<SpriteRenderer>().material.color = Color.white;
+            //This removes the Na clone created above//
+            Destroy(GameObject.Find("Na(Clone)"));
 		}
 	}
 }
